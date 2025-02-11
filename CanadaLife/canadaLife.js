79a0e2d2a3720224672248b3f1ed7f52ca076028
Thibault Friedrich <thibault.friedrich@gmail.com>
2025-02-10 21:33:19 -0500

    feat: add security

---
+++ CanadaLife/canadaLife.js
@@ -0,0 +1,36 @@
+$(document).ready(function () {
+  var secured = false
+  $('#askPasswordModal').modal({ backdrop: 'static' })
+
+  $('#askPasswordModal').on('hidden.bs.modal', function () {
+    if (secured) return
+    $('#askPasswordModal').modal('show')
+  })
+
+  $('#passwordError').hide()
+
+  $('#formPassword').submit(function (e) {
+    e.preventDefault()
+    if ($('#password').val() === 'CanadaLife') {
+      secured = true
+      $('#askPasswordModal').modal('hide')
+      $.get('canadaLife-private.html').then(data => {
+        $('#secure').append(data)
+      })
+    } else {
+      $('#passwordError').show()
+    }
+  })
+
+  // $('#openCanadaLife').click(function () {
+  //   if ($('#password').val() === 'CanadaLife') {
+  //     secured = true
+  //     $('#askPasswordModal').modal('hide')
+  //     $.get('canadaLife-private.html').then(data => {
+  //       $('#secure').append(data)
+  //     })
+  //   } else {
+  //     $('#passwordError').show()
+  //   }
+  // })
+})
